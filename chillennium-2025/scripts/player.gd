397381cba83e6c071309de9a6d190dbc6490f482
Aurora Jitrskul <aurawra@outlook.com>
2025-02-07 22:33:30 -0600

    Updated skeleton code

---
+++ chillennium-2025/scripts/player.gd
@@ -0,0 +1,35 @@
+class_name Player extends CharacterBody2D
+
+const SPEED = 8000.0
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+func _physics_process(delta: float) -> void:
+	#create 2d vector
+	var direction = Vector2.ZERO
+	
+	#check for player input
+	if Input.is_action_pressed("move_up"):
+		direction.y -= 1
+	if Input.is_action_pressed("move_down"):
+		direction.y += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1 
+
+	#normalizes vector
+	if direction != Vector2.ZERO:
+		direction = direction.normalized()	
+	
+	if direction.x != 0:
+		if direction.x > 0:
+			sprite.play("right")
+		else:
+			sprite.play("left")
+	elif direction.y < 0:
+		sprite.play("up")
+	elif direction.y > 0:
+		sprite.play("down")
+	
+	velocity = direction * SPEED * delta
+	move_and_slide()
