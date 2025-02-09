1cabf30e1511908a8978f83d394102fa729fa3dc
avoormolen <andrewvoormolen@outlook.com>
2025-02-09 02:07:13 -0600

    Made level 3

---
+++ chillennium-2025/scripts/sign_2.gd
@@ -0,0 +1,10 @@
+extends Sprite2D
+
+@onready var animation_player: AnimationPlayer = $"../AnimationPlayer"
+
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_pressed("interact"):
+		var PlayerPosition: Vector2 = get_parent().get_node("Player").position - position
+		if PlayerPosition.length() < 300:
+			animation_player.play("Sign2Disp")
