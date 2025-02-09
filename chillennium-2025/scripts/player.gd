f37e6e1a05098fcc5f45c0f231cd584796c2c6ed
Aurora Jitrskul <aurawra@outlook.com>
2025-02-08 22:04:28 -0600

    Finished most of level 1

--- chillennium-2025/scripts/player.gd
+++ chillennium-2025/scripts/player.gd
@@ -6,7 +6,10 @@ const SPEED = 32000.0
 @onready var yellow_lantern: PointLight2D = $"yellow-lantern"
 @onready var red_lantern: PointLight2D = $"red-lantern"
 @onready var blue_lantern: PointLight2D = $"blue-lantern"
-@onready var current_lantern: PointLight2D = yellow_lantern
+@onready var current_lantern_node: PointLight2D = yellow_lantern
+var current_lantern_num: int = 0
+
+
 var has_red: bool = false
 var has_blue: bool = false
 
@@ -24,24 +27,27 @@ func deathfunc() -> void:
 func _input(event) -> void:
 	if event.is_action_pressed("leave_game"):
 		get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
-	elif current_lantern != yellow_lantern and Input.is_action_pressed("swap_yellow"):
-		if current_lantern == red_lantern:
+	elif current_lantern_node != yellow_lantern and Input.is_action_pressed("swap_yellow"):
+		if current_lantern_node == red_lantern:
 			animation_player.play("red-to-yellow")
 		else:
 			animation_player.play("blue-to-yellow")
-		current_lantern = yellow_lantern
-	elif current_lantern != red_lantern and has_red == true and Input.is_action_just_pressed("swap_red"):
-		if current_lantern == yellow_lantern:
+		current_lantern_node = yellow_lantern
+		current_lantern_num = 0
+	elif current_lantern_node != red_lantern and has_red == true and Input.is_action_just_pressed("swap_red"):
+		if current_lantern_node == yellow_lantern:
 			animation_player.play("yellow-to-red")
 		else:
 			animation_player.play("blue-to-red")
-		current_lantern = red_lantern
-	elif current_lantern != blue_lantern and has_blue == true and Input.is_action_just_pressed("swap_blue"):
-		if current_lantern == yellow_lantern:
+		current_lantern_node = red_lantern
+		current_lantern_num = 1
+	elif current_lantern_node != blue_lantern and has_blue == true and Input.is_action_just_pressed("swap_blue"):
+		if current_lantern_node == yellow_lantern:
 			animation_player.play("yellow-to-blue")
 		else:
 			animation_player.play("red-to-blue")
-		current_lantern = blue_lantern
+		current_lantern_node = blue_lantern
+		current_lantern_num = 2
 
 func _physics_process(delta: float) -> void:
 	
@@ -67,16 +73,16 @@ func _physics_process(delta: float) -> void:
 	if direction.x != 0:
 		if direction.x > 0: #right
 			sprite.rotation_degrees = -90
-			current_lantern.position = Vector2(85, -40)
+			current_lantern_node.position = Vector2(85, -40)
 		else: #left
 			sprite.rotation_degrees = 90
-			current_lantern.position = Vector2(-85, -40)
+			current_lantern_node.position = Vector2(-85, -40)
 	elif direction.y < 0: #up
 		sprite.rotation_degrees = -180
-		current_lantern.position = Vector2(-85, -40)
+		current_lantern_node.position = Vector2(-85, -40)
 	elif direction.y > 0: #down
 		sprite.rotation_degrees = 0
-		current_lantern.position = Vector2(40, 85)
+		current_lantern_node.position = Vector2(40, 85)
 	
 	velocity = direction * SPEED * delta
 	move_and_collide(direction)
