d507d11bc7154946d241ee54c4e57b37bfc9d688
avoormolen <andrewvoormolen@outlook.com>
2025-02-10 12:42:41 -0600

    final changes

--- pixieslabyrinth/chillennium-2025/scripts/blue_lantern.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
-
-func _on_body_entered(body: Node2D) -> void:
-	if body.name == "Player":
-		body.has_blue = true
-		animation_player.play("pick_up")
-		
--- pixieslabyrinth/chillennium-2025/scripts/l3__moveable_wall.gd
+++
@@ -1,6 +0,0 @@
-extends AnimatableBody2D
-
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
-
-func open():
-	animation_player.play("door_move")
